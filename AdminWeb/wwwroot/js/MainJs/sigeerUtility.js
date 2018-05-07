d9505c30d986ca3a5e16929b57f5c9faa64baa02
sigeer <1452069418@qq.com>
2018-05-07 19:40:19 +0800

    Revert "0507"
    This reverts commit 4d53dfd7cd8b05868e434f247d4b1a87df99f54d.

--- AdminWeb/wwwroot/js/MainJs/sigeerUtility.js
+++
@@ -1,7 +0,0 @@
-﻿var apiUrl = 'http://localhost:8082';
-String.prototype.isNullOrEmpty = function () {
-    return this == null || this == "";
-}
-String.isNullOrEmpty = function (str) {
-    return str == null || str == "";
-}
