542d2a0757edc472f79498cef769d22df9746e15
sigger <1452069418@qq.com>
2018-08-06 14:55:25 +0800

    update - 02

--- AdminViews/ClientApp/src/app/content/content.module.ts
+++ AdminViews/ClientApp/src/app/content/content.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { routes } from './route';
 import { LoginComponent } from './page/login/login.component'
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forRoot(routes),
   ],
   declarations: [
--- AdminViews/ClientApp/src/app/content/page/login/login.component.ts
+++ AdminViews/ClientApp/src/app/content/page/login/login.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private apiUrl :string = 'http://localhost:8092/';
   public form: loginModel = {
     userid: 'admin',
     password: '111111'
   };
   constructor(private http: HttpClient) { }
   login() {
-    this.http.post('http://localhost:8092/' + 'api/Identity/GetToken', this.form).subscribe(result => {
+    this.http.post(this.apiUrl + 'api/Identity/GetToken', this.form).subscribe(result => {
       alert(result);
     }, error => alert(JSON.stringify(error)));
   }
